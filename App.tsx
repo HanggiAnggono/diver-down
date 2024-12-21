7b53ebb06b3221534eca10542ee7790836fcc7eb
Hanggi Anggono <anggonohanggi@gmail.com>
2024-12-21 16:59:24 +0700

    add flex and update global.css

--- App.tsx
+++ App.tsx
@@ -9,17 +9,36 @@ import './global.css';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import {Button} from '~/components/ui/button';
+import {Flex} from '~/components/ui/flex';
 import {Text} from '~/components/ui/text';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-      <Text className="text-6xl">App.tsx</Text>
-      <Button>
-        <Text>Button</Text>
-      </Button>
-      <Text className="p-4 pt-10 text-6xl text-primary">App.tsx</Text>
+      <Flex className="flex-col gap-2 p-2">
+        <Button>
+          <Text>Button</Text>
+        </Button>
+        <Button className="bg-secondary">
+          <Text className="text-secondary-foreground">Button</Text>
+        </Button>
+        <Button className="bg-accent">
+          <Text className="text-accent-foreground">Button</Text>
+        </Button>
+        <Button className="bg-destructive">
+          <Text>Button</Text>
+        </Button>
+        <Button className="bg-muted">
+          <Text className="text-muted-foreground">Button</Text>
+        </Button>
+        <Button className="bg-primary">
+          <Text>Button</Text>
+        </Button>
+        <Button className="bg-foreground">
+          <Text>Button</Text>
+        </Button>
+      </Flex>
     </SafeAreaView>
   );
 }
