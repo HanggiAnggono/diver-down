9ae2af96235ebdcfe27ab8df005b8eac9629cbd9
Hanggi Anggono <anggonohanggi@gmail.com>
2024-12-21 16:25:54 +0700

    init

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          '~': '.',
+        },
+      },
+    ],
+  ],
 };
